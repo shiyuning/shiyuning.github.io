2541a5602849c8364e44109c9a28fd65f932dfc7
Yuning Shi <shiyuning@gmail.com>
2018-08-30 10:35:25 -0400

    Update talk map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,27 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "University Park, PA",
+    40.798246,
+    -77.859833
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Austin, TX",
+    30.263652,
+    -97.739617
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Arlington, VA",
+    38.801654,
+    -77.070449
+  ]
+  [
+    "San Francisco, CA",
+    37.784374,
+    -122.400701
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "New Orleans, LA",
+    29.943335,
+    -90.064083
   ]
 ];
